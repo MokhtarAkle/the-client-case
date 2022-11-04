17f4b8fd264e6e88d668b73429308c3497dfcd48
MokhtarAkle <mokhtar.akle@hva.nl>
2022-11-04 04:19:24 +0100

    infowindow search styling

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,14 @@ var populateSize = document.querySelectorAll(".sizeJSON");
 var populateUse = document.querySelectorAll(".useJSON");
 var checkboxes = document.querySelectorAll("input[type=checkbox]");
 var sortSize = document.querySelector(".sortSize");
+var sortUse = document.querySelector(".sortUse");
+var sortFunc = document.querySelector(".sortFunc");
+var sortLoc = document.querySelector(".sortLoc");
+var sortStyling = document.querySelector(".sortHidden");
+var sortButton = document.querySelector("#sort-button");
+var filterStyling = document.querySelector(".filterHidden");
+var filterButton = document.querySelector("#filter-button");
+
 const media1 = window.matchMedia('(max-width: 980px)');
 const media2 = window.matchMedia('(min-width: 980px)');
 
@@ -95,7 +103,7 @@ function initMap() {
 
 async function setMarkers(map){
     
-    const svgMarker = {
+    const svgMarkerBig = {
       path: "M 10 10 H 140 V 70 H 10 L 10 10",
       fill: "",
       fillOpacity: 0.2,
@@ -106,6 +114,28 @@ async function setMarkers(map){
       anchor: new google.maps.Point(0, 0),
       };
 
+      const svgMarkerSmall = {
+        path: "M 10 10 H 90 V 90 H 10 L 10 10",
+        fill: "",
+        fillOpacity: 0.2,
+        strokeWeight: 2,
+        strokeColor: "#4F52C9",
+        rotation: -20,
+        scale: 0.5,
+        anchor: new google.maps.Point(0, 0),
+        };
+
+        const svgMarkerMedium = {
+          path: "M 10 10 H 120 V 90 H 10 L 10 10",
+          fill: "",
+          fillOpacity: 0.2,
+          strokeWeight: 2,
+          strokeColor: "#4F52C9",
+          rotation: -20,
+          scale: 0.5,
+          anchor: new google.maps.Point(0, 0),
+          };
+
         const requestURL = './smartzones.json';
         const request = new Request(requestURL);
       
@@ -113,10 +143,22 @@ async function setMarkers(map){
         const smartzones = await response.json();
     
       
-      for (let i = 0; i < smartzones.length; i++){        
+      for (let i = 0; i < smartzones.length; i++){
+        
+        function markerSelect(){
+          if(smartzones[i].size == "1 plek"){
+            return svgMarkerSmall
+          }
+          if(smartzones[i].size == "1,5 plek"){
+            return svgMarkerMedium;
+          }
+          else
+          return svgMarkerBig;
+        }
+
       const marker = new google.maps.Marker({
           position: {lat: parseFloat(smartzones[i].lat), lng: parseFloat(smartzones[i].lon)},
-          icon: svgMarker,
+          icon: markerSelect(),
           map: map,
           title: smartzones[i].location,
         });
@@ -126,7 +168,8 @@ async function setMarkers(map){
                               "<br> <br>" + "Locatie: " + smartzones[i].location +
                               "<br> <br>" + "Functie: " + smartzones[i].function + " | <br>" + smartzones[i].function1 + " | " + smartzones[i].function2 +
                               "<br> <br>" + "Grootte: " + smartzones[i].size +
-                              "<br> <br>" + "Gebruik: " + smartzones[i].utilization;
+                              "<br> <br>" + "Gebruik: " + smartzones[i].utilization +
+                              "<br> <br>" + "<a href='http://maps.google.com/maps?saddr=52.362440594307465,4.915010541817515&daddr=" + smartzones[i].lat + "," + smartzones[i].lon +"'><img class='directionsButton' src='./assets/directions.svg'></a>";
     
         marker.addListener("click", () => {
           infoWindow.close();
@@ -177,22 +220,71 @@ function populate(){
 
 populate();
 
-sortSize.addEventListener("click", () =>{
-  smartzones.sort((a, b) => {
-const sizeA = a.size.toUpperCase(); // ignore upper and lowercase
-const sizeB = b.size.toUpperCase(); // ignore upper and lowercase
-if (sizeA < sizeB) {
-return -1;
+function initSort(){
+
+
+  sortSize.addEventListener("click", () =>{
+    smartzones.sort((a, b) => {
+  const sizeA = a.size.toUpperCase(); 
+  const sizeB = b.size.toUpperCase(); 
+  if (sizeA < sizeB) {
+  return -1;
+  }
+  if (sizeA > sizeB) {
+  return 1;
+  }
+  populate();
+  return 0;
+  });
+  });
+
+  sortUse.addEventListener("click", () =>{
+    smartzones.sort((a, b) => {
+  const useA = a.utilization.toUpperCase(); 
+  const useB = b.utilization.toUpperCase(); 
+  if (useA < useB) {
+  return -1;
+  }
+  if (useA > useB) {
+  return 1;
+  }
+  populate();
+  return 0;
+  });
+  });
+
+  sortFunc.addEventListener("click", () =>{
+    smartzones.sort((a, b) => {
+  const funcA = a.function.toUpperCase();
+  const funcB = b.function.toUpperCase();
+  if (funcA < funcB) {
+  return -1;
+  }
+  if (funcA > funcB) {
+  return 1;
+  }
+  populate();
+  return 0;
+  });
+  });
+
+  sortLoc.addEventListener("click", () =>{
+    smartzones.sort((a, b) => {
+  const locA = a.town.toUpperCase();
+  const locB = b.town.toUpperCase();
+  if (locA < locB) {
+  return -1;
+  }
+  if (locA > locB) {
+  return 1;
+  }
+  populate();
+  return 0;
+  });
+  });
 }
-if (sizeA > sizeB) {
-return 1;
-}
-populate();
-populateButtons();
-return 0;
-});
-console.log(smartzones)
-});
+initSort();
+
 
 }
   
@@ -218,6 +310,29 @@ function searchShow(){
   }
   else{
     searchBar.style.display = "none";
+  }
+}
+
+
+function sortShow(){
+  if(sortStyling.classList.contains("sortHidden")){
+    sortStyling.classList.toggle("sortStyle");
+    sortStyling.classList.remove("sortHidden");
+  }
+  else{
+    sortStyling.classList.toggle("sortStyle");
+    sortStyling.classList.add("sortHidden");
+  }
+}
+
+function filterShow(){
+  if(filterStyling.classList.contains("filterHidden")){
+    filterStyling.classList.toggle("filterStyle");
+    filterStyling.classList.remove("filterHidden");
+  }
+  else{
+    filterStyling.classList.toggle("filterStyle");
+    filterStyling.classList.add("filterHidden");
   }
 }
 
@@ -259,6 +374,9 @@ window.initMap = initMap;
 filterTest();
 buttonSlide.addEventListener("click", classSlide);
 searchIcon.addEventListener("click", searchShow);
+sortButton.addEventListener("click", sortShow);
+filterButton.addEventListener("click", filterShow);
+
 // https://developers.google.com/maps/documentation/javascript
 // https://support.google.com/mymaps/answer/3024454?hl=en&co=GENIE.Platform%3DDesktop#:~:text=Create%20a%20map,map%20a%20name%20and%20description.
 // https://stackoverflow.com/questions/41648702/prevent-marker-from-scaling-when-zoom-out
